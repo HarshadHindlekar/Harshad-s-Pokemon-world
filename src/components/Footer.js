34c2ff4c56e1f245d66aa3041a7b6f0170b1fd65
Harshad Hindlekar <harshad@cloudfinch.com>
2025-08-16 20:50:40 +0530

    feat: add footer component and modern Pokemon-themed styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
       <div className="frame">
         <h1 className="frame__title">Harshad's Pokemon World</h1>
         <span className="frame__credits">
-          Copyright 2024.<br /> All Rights Reserved By{' '}
+          Copyright {new Date().getFullYear()}.<br /> All Rights Reserved By{' '}
           <a href="https://harshad-portfolio.vercel.app/">Harshad Hindlekar</a>
         </span>
       </div>
